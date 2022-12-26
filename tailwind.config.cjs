ea761a66d3e752ca79d8284052bf978cb456ca02
mhbaando <maxamuudyare21@gmail.com>
2022-12-26 22:25:11 +0300

    ci: added new colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        ...colors,
+        primaryBlue: '#1967D2',
+        primaryDark: '#0146A6',
+        secondaryYellow: '#F9AB00',
+        secondaryDark: '#E9A000',
+        whiteBlue: '#EFF4FC',
+        overlay: '#202124'
+      },
+      fontFamily: {
+        Roboto: ['Nunito Sans', 'sans-serif']
+      }
+    }
   },
   plugins: []
 }
