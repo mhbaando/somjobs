c0da818e4d820ab8fe777cd4dbee18b08e3872af
mhbaando <maxamuudyare21@gmail.com>
2022-12-28 10:35:45 +0300

    ci: implemented the call to action

---
+++ src/ui/molecules/CallToAction/index.tsx
@@ -0,0 +1,34 @@
+import CTA from '@/assets/misc/cta.png'
+import Button from '@/shared/components/Button'
+import { useNavigate } from 'react-router-dom'
+const CallToAction = (): React.ReactElement => {
+  const navigate = useNavigate()
+  return (
+    <section className='relative w-full mt-24 flex items-center justify-center h-[450px]'>
+      <img
+        src={CTA}
+        alt='Call to action'
+        className='absolute top-0 w-full h-full object-cover object-center'
+      />
+      <div className='section relative z-10 h-full '>
+        <div className='container flex flex-col items-center justify-center h-full'>
+          <h2 className='text-2xl sm:text-3xl font-semibold text-white text-center'>
+            Make a Difference with Your Online Resume!
+          </h2>
+          <p className='text-white text-center'>
+            Your resume in minutes with Somjobs resume assistant is ready!
+          </p>
+          <Button
+            type='button'
+            additionalClasses='!bg-white  !text-primaryDark rounded-md py-2 px-5 mt-7'
+            handleClick={() => navigate('/register')}
+          >
+            Create an Account
+          </Button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default CallToAction
