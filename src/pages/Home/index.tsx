bf254f28e3de50e956a92210f4193775f30e2e69
mhbaando <maxamuudyare21@gmail.com>
2022-12-28 10:37:49 +0300

    refactor: refactored home page to host more components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,8 @@
+import AdForCompany from '@/ui/molecules/AdForCompany'
+import CallToAction from '@/ui/molecules/CallToAction'
 import Categories from '@/ui/molecules/Categories'
+import FeaturedJobs from '@/ui/molecules/FeaturedJobs'
+import TopCompany from '@/ui/molecules/TopCompanies'
 import Clients from '@/ui/molecules/clients/Index'
 import Layout from '@/ui/organisims/Layout'
 
@@ -7,6 +11,10 @@ const Home = (): React.ReactElement => {
     <Layout>
       <Clients />
       <Categories />
+      <FeaturedJobs />
+      <CallToAction />
+      <TopCompany />
+      <AdForCompany />
     </Layout>
   )
 }
