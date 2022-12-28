1b74d0e9f1c92d3df7b97a1c4ce18cd7c3f0f32c
mhbaando <maxamuudyare21@gmail.com>
2022-12-28 10:33:35 +0300

    refactor: refactored category card

--- src/components/Cards/Categories/index.tsx
+++ src/components/Cards/Categories/index.tsx
@@ -7,7 +7,7 @@ interface ICategoryCard {
 const CategoryCard: React.FC<ICategoryCard> = ({ name, jobs, imgSrc }): React.ReactElement => {
   return (
     <div className='flex flex-col items-center justify-center bg-white border hover:bg-whiteBlue hover:shadow-md transition-all duration-300 ease-in-out rounded-md h-[240px] cursor-pointer'>
-      <div className='flex w-16 h-16 bg-whiteBlue rounded-full items-center justify-center '>
+      <div className='flex w-16 h-16 bg-blue-100 rounded-full items-center justify-center '>
         <img src={imgSrc} alt={name} />
       </div>
       <h3 className='text-lg font-semibold mt-3'>{name}</h3>
