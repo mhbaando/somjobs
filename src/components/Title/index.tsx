dfc3baacdc38a3c973965eb6310aff07c648d056
mhbaando <maxamuudyare21@gmail.com>
2022-12-28 10:32:58 +0300

    ci: made it highly configurable

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,13 +1,18 @@
 interface ITitle {
   heading: string
   desc: string
+  align?: string
 }
-const Title: React.FC<ITitle> = ({ heading, desc }): React.ReactElement => {
+const Title: React.FC<ITitle> = ({ heading, desc, align }): React.ReactElement => {
   return (
     <section className='section'>
-      <div className='container w-full flex flex-col items-center justify-center'>
-        <h2 className='text-3xl font-semibold mb-3'>{heading}</h2>
-        <p className='text-sm text-gray-500'>{desc}</p>
+      <div
+        className={`max-w-6xl mx-auto w-full flex flex-col items-${
+          align != null ? align : 'center'
+        } justify-center`}
+      >
+        <h2 className='text-2xl sm:text-3xl font-semibold mb-3 '>{heading}</h2>
+        <p className={`text-sm text-gray-500 text-${align != null ? align : 'center'}`}>{desc}</p>
       </div>
     </section>
   )
