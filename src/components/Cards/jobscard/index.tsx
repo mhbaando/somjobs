34ca06b99b1a6d2222af0f43546fe7c59e428ab9
mhbaando <maxamuudyare21@gmail.com>
2022-12-30 20:39:10 +0300

    ci: registered new compont for jobs

--- src/components/Cards/jobscard/index.tsx
+++ src/components/Cards/jobscard/index.tsx
@@ -1,7 +1,71 @@
-import React from 'react'
-
-const JobsCard = (): React.ReactElement => {
-  return <div>JobsCard</div>
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { BsBriefcase, BsClockHistory, BsCashCoin } from 'react-icons/bs'
+import { SlLocationPin } from 'react-icons/sl'
+import Formatter from '@utils/Formatter'
+interface IJobCard {
+  jobName: string
+  companyName: string
+  location: string
+  postedDate: string
+  sallery?: number
+  shift: string
+  status?: string
+  companyImage: string
+}
+const JobCard: React.FC<IJobCard> = ({
+  jobName,
+  companyImage,
+  companyName,
+  location,
+  postedDate,
+  sallery,
+  shift,
+  status
+}): React.ReactElement => {
+  return (
+    <div className='border bg-white hover:bg-whiteBlue duration-300 transition-all ease-in-out cursor-pointer rounded-md w-full px-6 py-5'>
+      <div className='flex items-start gap-5'>
+        <div className='w-14  rounded-full overflow-hidden flex items-center justify-center '>
+          <img src={companyImage} alt='logo' className='object-cover object-center h-full' />
+        </div>
+        <div className='flex flex-col items-start'>
+          <h3 className='text-lg font-semibold'>{jobName}</h3>
+          <div className='flex items-center justify-start gap-2 flex-wrap mt-3'>
+            <p className='flex items-center gap-2 text-sm text-gray-500'>
+              <span>
+                <BsBriefcase />
+              </span>
+              {companyName}
+            </p>
+            <p className='flex items-center gap-2 text-sm text-gray-500'>
+              <span>
+                <SlLocationPin />
+              </span>
+              {location}
+            </p>
+            <p className='flex items-center gap-2 text-sm text-gray-500'>
+              <span>
+                <BsClockHistory />
+              </span>
+              {postedDate}
+            </p>
+            <p className='flex items-center gap-2 text-sm text-gray-500'>
+              <span>
+                <BsCashCoin />
+              </span>
+              {sallery ? Formatter.format(sallery) : 'Not Provided'}
+            </p>
+          </div>
+          <div className='flex items-center justify-start gap-3 w-full mt-4'>
+            {status != null && (
+              <div className='rounded-full px-4 text-sm py-1 bg-orange-100'>{status}</div>
+            )}
+            <div className='rounded-full px-4 text-sm py-1 bg-blue-100'>{shift}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default JobsCard
+export default JobCard
