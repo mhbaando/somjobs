7b22344ddbe34eb41a64e611e75bc3f2320710f6
mhbaando <maxamuudyare21@gmail.com>
2022-12-30 10:03:24 +0300

    fix: fiex z-index of the logo

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -10,7 +10,7 @@ const Login = (): React.ReactElement => {
       <div className='section max-w-6xl mx-auto w-full'>
         {/* Logo */}
         <Link to='/'>
-          <img src={Logo} alt='Logo white ' className='flex absolute top-5 left-7 w-24' />
+          <img src={Logo} alt='Logo white ' className='flex absolute top-5 left-7 w-24 z-10' />
         </Link>
         <img src={Bg} alt='' className='flex absolute top-0 left-0 w-screen h-screen z-0' />
         <LoginForm />
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -10,7 +10,11 @@ const Register = (): React.ReactElement => {
       <div className='section max-w-6xl mx-auto w-full'>
         {/* Logo */}
         <Link to='/'>
-          <img src={Logo} alt='Logo white ' className='flex absolute top-5 left-7 w-24' />
+          <img
+            src={Logo}
+            alt='Logo white '
+            className='flex absolute top-5 left-7 w-24 cursor-pointer'
+          />
         </Link>
         <img src={Bg} alt='' className='flex absolute top-0 left-0 w-screen h-screen z-0' />
         <RegisterForm />
