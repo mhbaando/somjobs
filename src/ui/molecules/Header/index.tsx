7e56e1c8c4abfc4870553fd1e4736922d0bd1c2b
mhbaando <maxamuudyare21@gmail.com>
2022-12-30 20:36:52 +0300

    refactor: remove some comments there

--- src/ui/molecules/Header/index.tsx
+++ src/ui/molecules/Header/index.tsx
@@ -2,7 +2,6 @@ import Hero from '@component/Hero/Hero'
 import DarkNav from '@component/Nav/Nav'
 
 const Header = (): React.ReactElement => {
-  // TODO: render dark nav only on home page
   return (
     <>
       <Hero />
