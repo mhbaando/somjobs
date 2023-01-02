961a6d2854b3be6cf969e7533b3898988d129f77
mhbaando <maxamuudyare21@gmail.com>
2023-01-02 09:40:12 +0300

    ci: implemented all our routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
+/* eslint-disable array-callback-return */
 import React, { Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
-import { publicRoutes } from './routes/routes'
-import Loader from './shared/components/Loader'
 import NotFound from '@error/404'
+import Loader from './shared/components/Loader'
+import { privateRoutes, publicRoutes } from './routes/routes'
 
 const App = (): React.ReactElement => {
   // TODO: impelement Guard and permission controll for the routes
+  const auth = true
   return (
     <Suspense fallback={<Loader />}>
       <Routes>
-        {publicRoutes.map(({ path, Element, nestedpath, NestedElement }) => {
-          if (nestedpath !== undefined && NestedElement !== undefined) {
+        {publicRoutes.map(({ path, Element, nested }) => {
+          if (nested.length > 0) {
             return (
               <Route key={path} path={path}>
                 <Route index element={<Element />} />
-                <Route path={nestedpath} element={<NestedElement />} />
+                {nested.map(({ path, Child }, i) => (
+                  <Route key={i} path={path} element={<Child />} />
+                ))}
               </Route>
             )
           }
           return <Route key={path} path={path} element={<Element />} />
+        })}
+        {/* privare routes */}
+        {privateRoutes.map(({ path, Element, nested }) => {
+          if (auth) {
+            if (nested.length > 0) {
+              return (
+                <Route key={path} path={path} element={<Element />}>
+                  {nested?.map(({ path, Child }, i) => (
+                    <Route key={i} path={path} element={<Child />} />
+                  ))}
+                </Route>
+              )
+            }
+            return <Route key={path} path={path} element={<Element />} />
+          }
         })}
         <Route path='*' element={<NotFound />} />
       </Routes>
