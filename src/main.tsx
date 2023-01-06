c8fb2dab2cc0a6ac549afd19a3b3ee9268ef46b5
mhbaando <maxamuudyare21@gmail.com>
2023-01-06 13:20:45 +0300

    refactor: refacotred for the use of rich text editor

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { render } from 'react-dom'
 import App from './App'
 import './styles/index.scss'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(
+render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 )
