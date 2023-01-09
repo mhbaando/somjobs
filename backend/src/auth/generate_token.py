46cdc5907be6bd2a41c28e2b563e344fe6980fb3
mhbaando <maxamuudyare21@gmail.com>
2023-01-10 01:30:25 +0300

    ci: made independe from the app

--- backend/src/auth/generate_token.py
+++ backend/src/auth/generate_token.py
@@ -1,17 +1,20 @@
 import jwt
+import os
 import datetime
 
-def generate_token(user,app):
+def generate_token(user):
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+
     payload = {
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=2, seconds=5),
             'iat': datetime.datetime.utcnow(),
             'sub': user.id,
             'role': user.role, 
             'email':user.email,
-            'company':'',
+            'owner':user.owner if user.owner else '',
         }
     return jwt.encode(
             payload,
-            app.config.get('SECRET_KEY'),
+            SECRET_KEY,
             algorithm='HS256'
         )
