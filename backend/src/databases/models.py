c803aa6ce6a675ae28583c58c3a791b432cec763
mhbaando <maxamuudyare21@gmail.com>
2023-01-11 11:35:51 +0300

    refactor: refactored the model

--- backend/src/databases/models.py
+++ backend/src/databases/models.py
@@ -1,7 +1,9 @@
 import os
-from flask_sqlalchemy import SQLAlchemy
-from uuid import uuid4
 from datetime import datetime
+from uuid import uuid4
+
+import sqlalchemy.dialects.postgresql as postgresql
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
@@ -9,14 +11,14 @@ def get_uuid():
     return uuid4().hex
 
 class Employee(db.Model):
-    __tablename__ = 'employee'
+    __tablename__ = "employee"
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     full_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(345), nullable=False)
     phone = db.Column(db.String(22), nullable=False)
-    title= db.Column(db.Text, nullable=False)
+    title = db.Column(db.Text, nullable=False)
     country = db.Column(db.String(9), nullable=False)
-    city = db.Column(db.String(12),nullable=False)
+    city = db.Column(db.String(12), nullable=False)
     summary = db.Column(db.Text)
     github_link = db.Column(db.Text)
     linkedin_link = db.Column(db.Text)
@@ -29,53 +31,61 @@ class Employee(db.Model):
 
     def format(self):
         return {
-            "id":self.id,
-            "full_name":self.full_name,
-            "email":self.email,
-            "phone" : self.phone,
-            "title" : self.title,
-            "country":self.country,
-            "city":self.city,
-            "summary":self.summary,
-            "github_link":self.github_link,
-            "linkedin_link":self.linkedin_link,
-            "image":self.image,
-            "cv_name":self.cv_name,
-            "cv":self.cv,
-            "company":self.company,
-            "created_at":self.created_at
+            "id": self.id,
+            "full_name": self.full_name,
+            "email": self.email,
+            "phone": self.phone,
+            "title": self.title,
+            "country": self.country,
+            "city": self.city,
+            "summary": self.summary,
+            "github_link": self.github_link,
+            "linkedin_link": self.linkedin_link,
+            "image": self.image,
+            "cv_name": self.cv_name,
+            "cv": self.cv,
+            "created_at": self.created_at,
         }
 
 
-
 class User(db.Model):
-    __tablename__ = 'user'
-    id = db.Column(db.String(64), primary_key=True, unique=True, default=get_uuid())
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(345), unique=True)
     password = db.Column(db.Text, nullable=False)
     role = db.Column(db.String(15), nullable=False)
-    owner =  db.Column(db.Integer, db.ForeignKey('employee.id'))
+    owner = db.Column(db.Integer, db.ForeignKey("employee.id"))
     created_at = db.Column(db.DateTime, default=datetime.now(), nullable=False)
+
+    def format(self):
+        return {
+            "user_id": self.id,
+            "user_email": self.email,
+            "user_created_at": self.created_at,
+            "user_role": self.role,
+        }
+
+
+class Company(db.Model):
+    __tablename__ = "company"
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    website = db.Column(db.String(100), nullable=False)
+    country = db.Column(db.String(9), nullable=False)
+    city = db.Column(db.String(12), nullable=False)
+    image = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.now(), nullable=False)
+    user = db.Column(db.Integer, db.ForeignKey("employee.id"))
 
 
     def format(self):
         return {
-            "user_id":self.id,
-            "user_email":self.email,
-            "user_created_at":self.created_at,
-            "user_role" : self.role,
+            "id": self.id,
+            "name": self.name,
+            "email":self.email,
+            "website":self.website,
+            "country": self.country,
+            "city": self.city,
+            "image": self.image,
         }
-
-
-
-class Company(db.Model):
-    __tablename__ = 'company'
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    name = db.Column(db.String(20),nullable=False)
-    email = db.Column(db.String(20),nullable=False)
-    website = db.Column(db.String(100),nullable=False)
-    country = db.Column(db.String(9),nullable=False)
-    city = db.Column(db.String(12),nullable=False)
-    image = db.Column(db.LargeBinary)
-    created_at = db.Column(db.DateTime, default=datetime.now(), nullable=False)
-    user = db.Column(db.Integer, db.ForeignKey('employee.id'))
